c59a911ceac29090b223245032b3413141069196
Eredin1789 <pelsytristan@gmail.com>
2024-03-04 16:27:27 +0100

    4commit

--- mdpcopy.bat
+++ mdpcopy.bat
@@ -1,17 +1,2 @@
-type nul > users.txt
-dir C:\Users* > users.txt
-type nul > users2.txt
-setlocal enabledelayedexpansion
-et 'file1=users.txt'
-set 'file2=users2.txt'
-for /f 'delims=' %%a in (!file1!) do (
-    set 'line=%%a'
-    set 'str=!line:~36!'
-    echo !str!>>!file2!
-)
-type nul > users3.txt
-setlocal enabledelayedexpansion
-set 'file1=users2.txt'
-set 'file2=users3.txt'
-set /a count=0
-for /f 'delims=' %%a in (!file1!) do (
+echo BONJOUE CA MARCGE !!!!
+pause
